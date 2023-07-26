c122a8640858b2c8def804e3b030628ee9b23768
tminas <thaisuckeman@yahoo.com.br>
2023-07-26 16:55:21 -0300

    feat: Ambiente curso jsx1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-function App() {
-  return <a href="https://www.origamid.com">Origamid</a>;
-}
+const titulo = <h1>Esse é um título</h1>;
+
+const App = () => {
+  const random = Math.random();
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return 'André ' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'Ford',
+    rodas: '4',
+  };
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>
+        {/* {true ? 'asdasd' : 'dsadas'} - {10} */}
+        {mostrarNome('Rafael')}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{(random * 1000) / 50}</p>
+    </>
+  );
+};
 
 export default App;
