f898dbc1e449ae80ea05f5e106d755aeac9cf01c
thaisquirino <thaisuckeman@yahoo.com.br>
2024-06-27 11:25:06 -0300

    feat:useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import React from 'react';
-import Produto from './Produto';
 
 const App = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [contar, setContar] = React.useState(0);
+  React.useEffect(() => {
+    console.log('Executou');
+  }, []);
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
-  }
-
-  return (
-    <div>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        smartphone
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
-    </div>
-  );
+  React.useEffect(() => {
+    document.title = 'Thaís Quirino' + contar;
+  }, [contar]);
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
-
 export default App;
