43839181ef822d307e50f4a922faf36cff23961e
thaisquirino <thaisuckeman@yahoo.com.br>
2024-06-27 11:39:43 -0300

    feat:useEffect exemplo de contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 const App = () => {
   const [contar, setContar] = React.useState(0);
-  React.useEffect(() => {
-    console.log('Executou');
-  }, []);
+  const [dados, setDados] = React.useState(null);
 
   React.useEffect(() => {
-    document.title = 'Thaís Quirino' + contar;
-  }, [contar]);
-  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((json) => setDados(json));
+  }, []);
+
+  return (
+    <div>
+      {dados && (
+        <div>
+          <h1>{dados.nome}</h1>
+          <p>R${dados.preco * contar}</p>
+        </div>
+      )}
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    </div>
+  );
 };
 export default App;
