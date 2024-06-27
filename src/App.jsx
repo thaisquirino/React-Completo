eeb510c3b04db0ee9663d8d7e550fc1ced4788e7
thaisquirino <thaisuckeman@yahoo.com.br>
2024-06-27 12:02:53 -0300

    feat:useEffect exemplo de eventos  com useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
+import Produto from './Produto';
 
 const App = () => {
-  const [contar, setContar] = React.useState(0);
-  const [dados, setDados] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
-      .then((response) => response.json())
-      .then((json) => setDados(json));
-  }, []);
+  const [ativo, setAtivo] = React.useState(false);
 
   return (
     <div>
-      {dados && (
-        <div>
-          <h1>{dados.nome}</h1>
-          <p>R${dados.preco * contar}</p>
-        </div>
-      )}
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
     </div>
   );
 };
---
+++ src/Produto.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Produto = () => {
+  React.useEffect(() => {
+    function handleScroll(event) {
+      console.log(event);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <div style={{ height: '200vh' }}>
+      <p>Meu Produto</p>
+    </div>
+  );
+};
+export default Produto;
