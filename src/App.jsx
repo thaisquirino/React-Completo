49e0e1e3525432294a79966f7bf98201e90cc5cd
thaisquirino <thaisuckeman@yahoo.com.br>
2024-07-05 20:52:05 -0300

    feat:useMemo e useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
-function operacaoLenta() {
-  let c;
-  for (let i = 0; i < 100000000; i++) {
-    c = i + i / 10;
-  }
-  return c;
-}
 
+const set1 = new Set();
+const set2 = new Set();
+
+const Produto = () => {
+  const func1 = () => {
+    console.log('Teste');
+  };
+
+  const func2 = React.useCallback(() => {
+    console.log('Teste');
+  }, []);
+
+  set1.add(func1);
+  set2.add(func2);
+
+  console.log('Set1:', set1);
+  console.log('Set2:', set2);
+
+  return (
+    <div>
+      <p onClick={func1}>Produto 1</p>
+      <p onClick={func2}>Produto 2</p>
+    </div>
+  );
+};
 const App = () => {
   const [contar, setContar] = React.useState(0);
 
-  // const valor = React.useMemo(() => {
-  //   const localItem = window.localStorage.getItem('produto');
-  //   console.log('Aconteceu o memo');
-  //   return localItem;
-  //}, []);
-  const t1 = performance.now();
-  const valor = React.useMemo(() => operacaoLenta(), []);
-
-  console.log(performance.now() - t1);
-  console.log(valor);
-
-  return <button onClick={() => setContar + 1}>{contar} </button>;
+  return (
+    <div>
+      <Produto />
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    </div>
+  );
 };
 
 export default App;
